ad0d132a8edce92a7f00fdfd4c9115b00b559785
Igor Polynets <polinom100@gmail.com>
2012-04-18 23:59:56 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+requires = []
+
+setup(
+    name = "bunny_xbmc",
+    description='eTV censore processor',
+    version = "0.1",
+    author = "Polynets Igor",
+    packages=['bunny']
+    include_package_data=True,
+  )
